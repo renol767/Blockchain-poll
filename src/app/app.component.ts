dc92486b55912576aa7d602daa5755e79ec4e156
renol767 <72129061+renol767@users.noreply.github.com>
2021-07-28 19:28:01 +0700

    PollComponent view and Logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   showForm = false;
+
+  // Create Mock data
+  polls = [
+  {
+    question: 'Do You Need Blockchain System Now?',
+    image: 'https://images.bisnis-cdn.com/posts/2019/06/03/930240/blockchain.jpg',
+    votes: [0, 5, 7],
+    voted: true,
+  },
+  {
+    question: 'We Need Buy the ETH now?',
+    image: 'https://lintasdaerah.com/wp-content/uploads/2018/09/investasi-ethereum-lebih-potensial-dibandingkan-bitcoin.jpg',
+    votes: [0, 3, 6, 1],
+    voted: false,
+  }
+  ];
 }
